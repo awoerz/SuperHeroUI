0026b23b059456a6e46948f6385cd0357f39f1d3
Adam <adam.woerz@toyotatmh.com>
2024-05-21 09:45:30 -0400

    Pagination and Sorting working with API on test page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FormTestComponent } from './pages/form-test/form-test.component';
 import { SignalTestComponent } from './pages/signal-test/signal-test.component';
+import { PaginationTestComponent } from './pages/pagination-test/pagination-test.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'form-test', component: FormTestComponent },
-    { path: 'signal-test', component: SignalTestComponent}
+    { path: 'signal-test', component: SignalTestComponent},
+    { path: 'pagination-test', component: PaginationTestComponent}
 ];
--- src/app/pages/pagination-test/pagination-test.component.ts
+++ src/app/pages/pagination-test/pagination-test.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
+import { Hero } from '../../models/hero';
+
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-pagination-test',
   standalone: true,
-  imports: [],
+  imports: [ MatTableModule, MatPaginatorModule, MatSortModule ],
   templateUrl: './pagination-test.component.html',
   styleUrl: './pagination-test.component.scss'
 })
 export class PaginationTestComponent {
+  private _heroService = inject(HeroService)
+  heroServiceData: Hero[] = []
+  
+  displayedColumns = ['id', 'name', 'firstName', 'lastName', 'place']
+  dataSource = new MatTableDataSource<Hero>(this.heroServiceData);
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngOnInit() {
+    this.getHeroesFromService();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  getHeroesFromService() {
+    this._heroService.getHeroes().subscribe(res => {
+      if(res != undefined) {
+        this.heroServiceData = res;
+        this.dataSource.data = this.heroServiceData;
+      }
+    })
+  }
 }
